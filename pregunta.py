efd3d4545fcb515fc49d3dc375b1e35103d32073
merinsr21 <80010472+fmerino21@users.noreply.github.com>
2022-11-14 00:28:11 -0500

    1

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,43 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df_raw = open('clusters_report.txt')
+
+    raw_rows = []
+    for row in df_raw:
+        row = row.replace('\n', '')
+        raw_rows.append(row)
+
+    index = ['1','2','3','4','5','6','7','8','9','10','11','12','13']
+    key_words = []
+    porcentaje = []
+    cantidad = []
+    cluster = []
+    key_word = ""
+    for row in range(4, len(raw_rows)):
+        if len(raw_rows[row].split()) > 0:
+            if raw_rows[row].split()[0] in index:   
+                aux = raw_rows[row].split()    
+                cluster.append(int(aux[0]))
+                cantidad.append(int(aux[1]))
+                porcentaje.append(float(aux[2].replace(',','.')))
+            key_word += raw_rows[row][41:] + " "  
+        else:
+            key_word = key_word[:-1]
+            key_words.append(key_word)
+            key_word = ""
+
+    for i in range(len(key_words)):
+        key_words[i] = key_words[i].replace(" ", "*")
+        key_words[i] = key_words[i].replace("**", "*")
+        key_words[i] = key_words[i].replace("***", "*")
+        key_words[i] = key_words[i].replace("*", " ")
+
+    dict = {'cluster': cluster, 
+        'cantidad_de_palabras_clave': cantidad,
+        'porcentaje_de_palabras_clave': porcentaje,
+        'principales_palabras_clave': key_words}
+
+    df = pd.DataFrame(dict)
 
     return df
